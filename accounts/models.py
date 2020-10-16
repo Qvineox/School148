8c1b40ca5a715d77a64872ad35e1ceeb7b570a00
Qvineox <yarlrusman@gmail.com>
2020-10-17 01:29:36 +0300

    Models almost done

--- accounts/models.py
+++ accounts/models.py
@@ -2,12 +2,48 @@ from django.conf import settings
 from django.db import models
 
 
+class StudyGroups(models.Model):
+    grade = models.PositiveSmallIntegerField(null=False, blank=False, verbose_name='Номер')
+    symbol = models.CharField(max_length=1, null=False, blank=False, verbose_name='Буква')
+
+    # отношения
+    specialisation = models.ForeignKey('journal.Disciples', null=True, on_delete=models.CASCADE,
+                                       verbose_name='Специализация')
+
+
+MALE = 'M'
+FEMALE = 'Fe'
+OTHER = 'Etc'
+
+
+class CreativeGroups(models.Model):
+    title = models.CharField(max_length=20, null=False, blank=False, verbose_name='Название')
+    creation_date = models.DateTimeField(auto_now_add=True, null=False)
+    requirements = models.TextField(null=True, blank=False)
+
+
+class Parents(models.Model):
+    first_name = models.CharField(max_length=30, null=False, blank=False, verbose_name='Имя')
+    second_name = models.CharField(max_length=30, null=False, blank=False, verbose_name='Фамилия')
+    last_name = models.CharField(max_length=30, null=True, blank=False, verbose_name='Отчество')
+    gender = models.CharField(max_length=5, null=False, blank=False,
+                              choices=[('M', 'Male'), ('Fe', 'Female'), ('Etc', 'Other')], default=OTHER)
+    phone = models.TextField(null=True, blank=False, max_length=20, verbose_name='Телефон')
+    email = models.TextField(null=False, blank=False, max_length=30, verbose_name='Электронная почта')
+
+    # учетные данные
+    account = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, verbose_name='Аккаунт')
+    registered = models.BooleanField(null=False, default=False, verbose_name='Зарегистрирован')
+
+
 # данные школьников
 class Apprentices(models.Model):
     # личная информация
     first_name = models.CharField(max_length=30, null=False, blank=False, verbose_name='Имя')
     second_name = models.CharField(max_length=30, null=False, blank=False, verbose_name='Фамилия')
     last_name = models.CharField(max_length=30, null=True, blank=False, verbose_name='Отчество')
+    gender = models.CharField(max_length=5, null=False, blank=False,
+                              choices=[('M', 'Male'), ('Fe', 'Female'), ('Etc', 'Other')], default=OTHER)
     birth_date = models.DateField(null=False, blank=False, verbose_name='Дата рождения')
     phone = models.TextField(null=True, blank=False, max_length=20, verbose_name='Телефон')
     email = models.TextField(null=True, blank=False, max_length=30, verbose_name='Электронная почта')
@@ -17,13 +53,16 @@ class Apprentices(models.Model):
     active = models.BooleanField(null=False, default=False, verbose_name='Активность')
 
     # отношения
-    # study_group = models.ForeignKey(Study_groups, on_delete=models.CASCADE)
+    study_group = models.ForeignKey(StudyGroups, null=True, on_delete=models.CASCADE, verbose_name='Учебная группа')
 
-    # father = models.ForeignKey(Parents, on_delete=models.CASCADE)
-    # mother = models.ForeignKey(Parents, on_delete=models.CASCADE)
+    father = models.ForeignKey(Parents, null=True, on_delete=models.CASCADE, verbose_name='Отец', related_name='+')
+    mother = models.ForeignKey(Parents, null=True, on_delete=models.CASCADE, verbose_name='Мать', related_name='+')
+
+    creative_groups = models.ManyToManyField('accounts.CreativeGroups')
 
     # учетные данные
-    account = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, verbose_name='Аккаунт')
+    account = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, verbose_name='Аккаунт')
+    register_date = models.DateField(null=True, auto_now_add=True, verbose_name='Дата регистрации')
 
 
 # данные учителей
@@ -32,6 +71,8 @@ class Teachers(models.Model):
     first_name = models.CharField(max_length=30, null=False, blank=False, verbose_name='Имя')
     second_name = models.CharField(max_length=30, null=False, blank=False, verbose_name='Фамилия')
     last_name = models.CharField(max_length=30, null=True, blank=False, verbose_name='Отчество')
+    gender = models.CharField(max_length=5, null=False, blank=False,
+                              choices=[('M', 'Male'), ('Fe', 'Female'), ('Etc', 'Other')], default=OTHER)
     birth_date = models.DateField(null=False, blank=False, verbose_name='Дата рождения')
     phone = models.TextField(null=False, blank=False, max_length=20, verbose_name='Телефон')
     email = models.TextField(null=False, blank=False, max_length=30, verbose_name='Электронная почта')
@@ -41,18 +82,79 @@ class Teachers(models.Model):
     active = models.BooleanField(null=False, default=False, verbose_name='Активность')
 
     # отношения
-    # supervision_group = models.ForeignKey(Study_groups, on_delete=models.CASCADE, verbose_name='Кураторство')
-    # disciple = models.ForeignKey(Disciples, on_delete=models.CASCADE, verbose_name='Специализация')
+    supervision_group = models.ForeignKey(StudyGroups, null=True, on_delete=models.CASCADE, verbose_name='Кураторство')
+    disciple = models.ForeignKey('journal.Disciples', null=True, on_delete=models.CASCADE, verbose_name='Специализация')
 
     # учетные данные
-    account = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, verbose_name='Аккаунт')
+    account = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, verbose_name='Аккаунт')
+    registered = models.BooleanField(null=False, default=False, verbose_name='Зарегистрирован')
 
 
-# учебные группы
-class StudyGroups(models.Model):
-    grade = models.PositiveSmallIntegerField(null=False, blank=False, verbose_name='Номер')
-    symbol = models.CharField(max_length=1, null=False, blank=False, verbose_name='Буква')
+# данные персонала
+class Staff(models.Model):
+    first_name = models.CharField(max_length=30, null=False, blank=False, verbose_name='Имя')
+    second_name = models.CharField(max_length=30, null=False, blank=False, verbose_name='Фамилия')
+    last_name = models.CharField(max_length=30, null=True, blank=False, verbose_name='Отчество')
+    gender = models.CharField(max_length=5, null=False, blank=False,
+                              choices=[('M', 'Male'), ('Fe', 'Female'), ('Etc', 'Other')], default=OTHER)
+    birth_date = models.DateField(null=False, blank=False, verbose_name='Дата рождения')
+    phone = models.TextField(null=True, blank=False, max_length=20, verbose_name='Телефон')
+    email = models.TextField(null=False, blank=False, max_length=30, verbose_name='Электронная почта')
+    civ_id = models.CharField(null=False, blank=False, max_length=30, verbose_name='Документ')
 
-    # отношения
-    # specialisation = models.ForeignKey(Disciples, on_delete=models.CASCADE, verbose_name='Специализация')
-    supervisor = models.ForeignKey(Teachers, on_delete=models.CASCADE, verbose_name='Куратор')
+    # учетные данные
+    account = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, verbose_name='Аккаунт')
+    registered = models.BooleanField(null=False, default=False, verbose_name='Зарегистрирован')
+
+
+# управляющий коллектив
+class Managers(models.Model):
+    first_name = models.CharField(max_length=30, null=False, blank=False, verbose_name='Имя')
+    second_name = models.CharField(max_length=30, null=False, blank=False, verbose_name='Фамилия')
+    last_name = models.CharField(max_length=30, null=True, blank=False, verbose_name='Отчество')
+    gender = models.CharField(max_length=5, null=False, blank=False,
+                              choices=[('M', 'Male'), ('Fe', 'Female'), ('Etc', 'Other')], default=OTHER)
+    birth_date = models.DateField(null=False, blank=False, verbose_name='Дата рождения')
+    phone = models.TextField(null=True, blank=False, max_length=20, verbose_name='Телефон')
+    email = models.TextField(null=False, blank=False, max_length=30, verbose_name='Электронная почта')
+    civ_id = models.CharField(null=False, blank=False, max_length=30, verbose_name='Документ')
+
+    # организационная информация
+    position = models.CharField(max_length=30, null=False, blank=False,
+                                choices=[
+                                    # директор
+                                    ('PR', 'Principal'),
+                                    # заместитель по воспитательной работе
+                                    ('DEW', 'Deputy of Educational Work'),
+                                    # заместитель по безопасности
+                                    ('DS', 'Deputy of Security'),
+                                    # заместитель по административно-хозяйственной части
+                                    ('DEA', 'Deputy of Administration and Economics'),
+                                    # предметный методист
+                                    ('SM', 'Subject Methodist'),
+                                    # методист общего образовательного процесса
+                                    ('GM', 'General Methodist'),
+                                    # работник бухгалтерии
+                                    ('ADM', 'Accounting Department Member'),
+                                    # глава бухгалтерии
+                                    ('ADH', 'Head of Accounting Department'),
+                                    # член педагогического совета
+                                    ('EDM', 'Education Department Member'),
+                                    # глава педагогического совета
+                                    ('EDH', 'Head of Education Department'),
+                                    # глава родительского комитета
+                                    ('HPAC', 'Head of Parental Advice Council'),
+                                    # глава управляющего совета
+                                    ('GBM', 'Governing Council Member'),
+                                    # глава управляющего совета
+                                    ('GBH', 'Head of Governing Council'),
+                                    # агент профсоюза
+                                    ('LUA', 'Labor Union Agent'),
+                                    # наемный сотрудник
+                                    ('THW', 'Temporary Hired Worker')
+                                ], default='THW')
+
+    # учетные данные
+    account = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, unique=False,
+                                verbose_name='Аккаунт')
+    registered = models.BooleanField(null=False, default=False, verbose_name='Зарегистрирован')
--- journal/models.py
+++ journal/models.py
@@ -1,3 +1,84 @@
+import os
+
+from django.conf import settings
 from django.db import models
 
-# Create your models here.
+
+# таблица всех доступный предметов
+class Disciples(models.Model):
+    title = models.CharField(max_length=30, null=False, blank=False, verbose_name='Название')
+    start_grade = models.PositiveSmallIntegerField(null=False, blank=False, verbose_name='Начальный класс')
+    end_grade = models.PositiveSmallIntegerField(null=False, blank=False, verbose_name='Конечный класс')
+
+    # флаги
+    active = models.BooleanField(null=False, default=False, verbose_name='Активность')
+
+    # отношения
+    supervisor = models.ForeignKey('accounts.Managers', null=True, blank=False, on_delete=models.CASCADE,
+                                   related_name='+', verbose_name='Руководитель')
+    methodist = models.ForeignKey('accounts.Managers', null=True, blank=False, on_delete=models.CASCADE,
+                                  related_name='+', verbose_name='Методист')
+
+
+# все поставленные оценки
+class Mark(models.Model):
+    EXCELLENT = 5
+    GOOD = 4
+    NORMAL = 3
+    BAD = 2
+    WORST = 1
+
+    MARK_CHOICES = (
+        (EXCELLENT, 'Отлично'),
+        (GOOD, 'Хорошо'),
+        (NORMAL, 'Удовлетворительно'),
+        (BAD, 'Неудовлетворительно'),
+        (WORST, 'Плохо'),
+    )
+
+    value = models.SmallIntegerField(choices=MARK_CHOICES, null=False, verbose_name='Значение')
+
+    EXAM = 4
+    TEST = 3
+    CLASSWORK = 2
+    HOMEWORK = 1
+
+    WEIGHT_CHOICES = (
+        (EXAM, 'Контрольная работа'),
+        (TEST, 'Тест/Проверочная работа'),
+        (CLASSWORK, 'Работа на уроке'),
+        (HOMEWORK, 'Домашняя работа')
+    )
+
+    weight = models.SmallIntegerField(choices=WEIGHT_CHOICES, null=False, default=2, verbose_name='Вес')
+    comment = models.TextField(null=True, blank=False, verbose_name='Комментарий')
+    rating_date = models.DateTimeField(auto_now_add=True, null=False, verbose_name='Дата установки')
+    modify_date = models.DateTimeField(auto_now=True, null=False, verbose_name='Дата изменения')
+
+    # отношения
+    holder = models.ForeignKey('accounts.Apprentices', null=False, on_delete=models.CASCADE, verbose_name='Получатель')
+    appraiser = models.ForeignKey('accounts.Teachers', null=False, on_delete=models.CASCADE,
+                                  verbose_name='Постановитель')
+    # lesson = models.ForeignKey('schedule.Lessons', null=True, on_delete=models.CASCADE, verbose_name='Урок')
+
+
+def homework_path():
+    return os.path.join(settings.LOCAL_FILE_DIR, 'homeworks')
+
+
+# все домашние задания
+class Homework(models.Model):
+    text = models.TextField(null=False, blank=False, verbose_name='Текст домашнего задания')
+    file = models.FilePathField(path=homework_path, null=True, match=".*\.zip$|.*\.pdf$|.*\.png$|.*\.jpeg$",
+                                verbose_name='Файл домашнего задания')
+
+    # флаги
+    required = models.BooleanField(default=True, null=False, verbose_name='Обязательность выполнения')
+
+    # отношения
+    author = models.ForeignKey('accounts.Teachers', null=True, on_delete=models.CASCADE, verbose_name='Автор')
+    placement_lesson = models.ForeignKey('schedule.Lessons', null=False, on_delete=models.CASCADE,
+                                         related_name='+', verbose_name='Урок установки')
+    deadline_lesson = models.ForeignKey('schedule.Lessons', null=True, on_delete=models.CASCADE,
+                                        related_name='+', verbose_name='Урок сдачи')
+    deadline_time = models.DateTimeField(null=False, blank=False, verbose_name='Срок сдачи')
