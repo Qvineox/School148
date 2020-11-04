18e3d9142c8868e0e2336782c26c12e6c9a5955f
Qvineox <yarlrusman@gmail.com>
2020-11-04 21:27:15 +0300

    Journal page updates
    Took 1 hour 32 minutes

--- journal/services.py
+++ journal/services.py
@@ -1,8 +1,9 @@
 import datetime
 import logging
 
+from accounts.models import Teachers
 from accounts.services import get_profile_from_user
-from journal.models import Lessons
+from journal.models import Lessons, Disciples
 
 logger = logging.getLogger('database')
 
@@ -30,25 +31,71 @@ def get_schedule_for_user(user):
     return process_schedule_data_for_journal_view(this_week_lessons)
 
 
+class Object(object):
+    pass
+
+
 def process_schedule_data_for_journal_view(lessons_queryset):
-    lessons_dict = {
-        '0': [],
-        '1': [],
-        '2': [],
-        '3': [],
-        '4': [],
-        '5': [],
-        '6': []
-    }
+    lessons = Object()
+
+    setattr(lessons, 'monday', [])
+    setattr(lessons, 'tuesday', [])
+    setattr(lessons, 'wednesday', [])
+    setattr(lessons, 'thursday', [])
+    setattr(lessons, 'friday', [])
+    setattr(lessons, 'saturday', [])
 
     for lesson in lessons_queryset.order_by('date', 'order').values():
-        lessons_dict[str(lesson['date'].weekday())].append(
-            (lesson['active'],
-             lesson['subject_id'],
-             lesson['homework_id'],
-             lesson['teacher_id'],
-             lesson['auditory'],
-             lesson['id'],
-             ))
+        if lesson['date'].weekday() == 0:
+            lessons.monday.append(get_lesson_info(lesson))
+        elif lesson['date'].weekday() == 1:
+            lessons.tuesday.append(get_lesson_info(lesson))
+        elif lesson['date'].weekday() == 2:
+            lessons.wednesday.append(get_lesson_info(lesson))
+        elif lesson['date'].weekday() == 3:
+            lessons.thursday.append(get_lesson_info(lesson))
+        elif lesson['date'].weekday() == 4:
+            lessons.friday.append(get_lesson_info(lesson))
+        elif lesson['date'].weekday() == 5:
+            lessons.saturday.append(get_lesson_info(lesson))
 
-    return lessons_dict
+    # не спрашивайте что это
+    lessons.monday = lessons.monday + [None] * (8 - len(lessons.monday))
+    lessons.tuesday = lessons.tuesday + [None] * (8 - len(lessons.tuesday))
+    lessons.wednesday = lessons.wednesday + [None] * (8 - len(lessons.wednesday))
+    lessons.thursday = lessons.thursday + [None] * (8 - len(lessons.thursday))
+    lessons.friday = lessons.friday + [None] * (8 - len(lessons.friday))
+    lessons.saturday = lessons.saturday + [None] * (8 - len(lessons.saturday))
+
+    return lessons
+
+
+# возвращает ифнормацию о уроке, по умолчанию возвращает краткую информацию
+def get_lesson_info(lesson_object, full=False):
+    lesson_teacher = Teachers.objects.get(id=lesson_object['teacher_id'])
+    lesson_teacher_info = ("{0} {1}.{2}".format(lesson_teacher.second_name,
+                                                lesson_teacher.first_name[0],
+                                                lesson_teacher.last_name[0]))
+
+    lesson_subject = Disciples.objects.get(id=lesson_object['subject_id']).title
+    lesson_auditory = lesson_object['auditory']
+
+    lesson_color = 'red'
+
+    if lesson_object['homework_id'] is not None:
+        lesson_has_homework = True
+    else:
+        lesson_has_homework = False
+
+    lesson_info = Object()
+    setattr(lesson_info, 'subject', lesson_subject)
+    setattr(lesson_info, 'id', lesson_object['id'])
+    setattr(lesson_info, 'active', lesson_object['active'])
+    setattr(lesson_info, 'color', lesson_color)
+
+    if full:
+        setattr(lesson_info, 'teacher', lesson_teacher_info)
+        setattr(lesson_info, 'auditory', lesson_auditory)
+        setattr(lesson_info, 'has_homework', lesson_has_homework)
+
+    return lesson_info
--- journal/views.py
+++ journal/views.py
@@ -1,16 +1,10 @@
 from django.shortcuts import render
 
-
 # Create your views here.
-from accounts.services import get_profile_data
 from home.views import navbar_data
 from journal.services import *
 
 
 def show_journal(request):
-    data = navbar_data(request)
-
-    data.update(get_schedule_for_user(request.user))
-    return render(request, 'journal/apprentice_journal.html', {'navbar': navbar_data(request)})
-
-
+    return render(request, 'journal/apprentice_journal.html',
+                  {'navbar': navbar_data(request), 'lessons': get_schedule_for_user(request.user)})
