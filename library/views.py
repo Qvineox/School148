f6799d8bac5ea250dc4a04ff6fbba57324df40ac
w0u0w <homerjs92@gmail.com>
2020-12-24 14:12:00 +0300

    library upd

--- library/views.py
+++ library/views.py
@@ -3,13 +3,12 @@ from home.views import navbar_data
 from library.models import Books, LoanReceipts
 from journal.models import Disciples
 from accounts.services import get_profile_data, get_profile_from_user
-
+from library.forms import BookAddForm
 
 def show_library(request):
     lib_books = Books.objects.all()
-    bor_books = LoanReceipts.objects.all()
+    bor_books = LoanReceipts.objects.filter(borrower_id=request.user.id)
     person_data = navbar_data(request)
-
     return render(request, 'library/new_lib.html',
                   {'all_books': lib_books,
                    'person_data': person_data,
@@ -22,10 +21,23 @@ def book_borrow_page(request, pk):
     lib_book = Books.objects.get(ISBN_number=pk)
     disciples = Disciples.objects.all()
     person_data = navbar_data(request)
+    if request.method == 'POST':
+        form = BookAddForm(request.POST)
+        print(form)
+        print(form.errors)
+        if form.is_valid():
+            
+                form.save()
+                return redirect('library_page')
+    else:
+        print(pk)
+        form = BookAddForm(initial={'book': pk, 'borrower': request.user})
+
     return render(request, 'library/book_detail.html',
                   {
                       'book_detail': lib_book,
                       'navbar': person_data,
-                      'predmety': disciples
+                      'predmety': disciples,
+                      'add_book_form': form
                    }
                   )
